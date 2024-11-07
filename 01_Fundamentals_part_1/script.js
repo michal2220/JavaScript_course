809b86817fe0debd0fe2b298d0ec46f59a747027
Michal G <gawlas05@gmail.com>
2024-11-07 14:09:43 +0100

    Operators

--- 01_Fundamentals_part_1/script.js
+++ 01_Fundamentals_part_1/script.js
@@ -1,14 +1,53 @@
-// let js = "amazing";
-// console.log(40 + 8 + 23 - 10 - 50);
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10 - 50);
 
-// console.log("Jonas");
-// console.log("Jonas");
+console.log("Jonas");
+console.log("Jonas");
 
-// let firstName = "Jonas";
-// console.log(firstName);
+let firstName = "Jonas";
+console.log(firstName);
 
 
 let isItTrue = true;
 console.log(isItTrue);
 
 console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "Name")
+
+isItTrue = "YES!";
+console.log(isItTrue);
+console.log(typeof isItTrue);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+// let age = 28;
+// age = 29;
+
+// const birthYear = 1996;
+// // birthYear = 1995;
+
+// var job = "programmer";
+// var job = "logger";
+
+const now = 2037;
+const ageJonas = now - 1996;
+const ageSarah = now - 1999;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 2, "2 do 3=", 2 ** 3);
+
+const firstName = "Michal";
+const lastName = "Nazwisko";
+console.log(firstName + " " + lastName);
+
