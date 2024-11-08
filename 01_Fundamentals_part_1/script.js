95cd998ccddd7e9b333b6c9ebb79b77530a19c8e
Michal G <gawlas05@gmail.com>
2024-11-08 19:49:02 +0100

    Strings

--- 01_Fundamentals_part_1/script.js
+++ 01_Fundamentals_part_1/script.js
@@ -61,14 +61,26 @@ console.log(typeof null);
 // const isFullAge = ageSarah >= 18;
 // console.log(isFullAge);
 
-const now = 2037;
-const ageJonas = now - 1996;
-const ageSarah = now - 2019;
+// const now = 2037;
+// const ageJonas = now - 1996;
+// const ageSarah = now - 2019;
 
-console.log(now - 1996 > now - 2019);
+// console.log(now - 1996 > now - 2019);
 
-let x, y;
-x = y = 25 - 10 - 5;
-console.log(x, y);
-const avarageAge = (ageJonas + ageSarah) / 2;
-console.log(ageJonas, ageSarah, avarageAge);
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+// const avarageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, avarageAge);
+
+const firstName = "Michal";
+const job = "teacher";
+const birthYear = 1996;
+const year = 2037;
+
+const michal = "I'm " + firstName + ", I'm a " + job + " and I was born in " + birthYear + ". So I'm " + (year - birthYear) + " years old.";
+
+console.log(michal);
+
+const michalNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+console.log(michalNew)
