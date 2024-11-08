6111a95814a8393fb9eec4257e711aa151ee8465
Michal G <gawlas05@gmail.com>
2024-11-08 22:18:03 +0100

    Switch statements

--- 01_Fundamentals_part_1/script.js
+++ 01_Fundamentals_part_1/script.js
@@ -182,3 +182,29 @@ console.log(typeof null);
 // } else {
 //     console.log("Not good for driving")
 // }
+
+
+const day = "monday";
+
+switch (day) {
+    case "monday":
+        console.log("Plan course structure");
+        console.log("Go to coding meetup");
+        break;
+    case "tuesday":
+        console.log("Prepare videos");
+        break;
+    case "wednesday":
+    case "thursday":
+        console.log("Wirte examples");
+        break;
+    case "friday":
+        console.log("record videos");
+        break;
+    case "saturday":
+    case "sunday":
+        console.log("weekdend");
+        break
+    default:
+        console.log("Not a day");
+}
