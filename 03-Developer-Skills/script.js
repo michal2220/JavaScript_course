59ae188e41a2a45060a5fe75a809bb164d031256
Michal G <gawlas05@gmail.com>
2024-11-12 22:59:14 +0100

    Adding 3rd lesson

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+("use strict");
 
+const xyz = 23;
+const caclAge = (birthYear) => 2037 - birthYear;
+
+console.log(caclAge(1996));
