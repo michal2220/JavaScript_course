9f691712f0385869e72ea39517fb65248358ba94
Michal G <gawlas05@gmail.com>
2024-11-12 10:49:09 +0100

    Adding functions

--- 02_Fundamentals_part_2/script.js
+++ 02_Fundamentals_part_2/script.js
@@ -1,7 +1,44 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive");
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive");
+
+// function logger() {
+//     console.log("My name is Michal");
+// }
+
+
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJucie = fruitProcessor(5, 0)
+// console.log(appleJucie);
+
+
+// const friutJuice = fruitProcessor(2, 3)
+// console.log(friutJuice);
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1996);
+console.log(age1);
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(2000);
+console.log(age1, age2);
+
